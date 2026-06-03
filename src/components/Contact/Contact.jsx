82a06312f890238ae073406b3d165c89bf3c859e
raerawr <121990754+raerawr@users.noreply.github.com>
2026-06-03 01:06:29 -0500

    cleaned up code and removed unnecessary files

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -61,27 +61,5 @@ export const Contact = () => {
             </ul>
         </section>
     </div>
-
-    /*
-    <footer id="contact" className={styles.container}>
-        <div className={styles.text}>
-            <h2>Contact</h2>
-            <p>Let's Connect!</p>
-        </div>
-        <ul className={styles.links}>
-            <li className={styles.link}>
-                <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-                <a href="mailto: myemail@email.com">myemail@email.com</a>
-            </li>
-            <li className={styles.link}>
-                <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" />
-                <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
-            </li>
-            <li className={styles.link}>
-                <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-                <a href="https://www.github.com/myname">github.com/myname</a>
-            </li>
-        </ul>
-    </footer>*/
   )
 }
