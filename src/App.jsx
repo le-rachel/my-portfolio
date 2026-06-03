92d26ac6455a34028de471ee64190060e85e24cc
raerawr <121990754+raerawr@users.noreply.github.com>
2026-06-03 01:48:59 -0500

    footer implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Hero } from "./components/Hero/Hero";
 import { About } from './components/About/About';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <About />
     <Projects />
     <Contact />
+    <Footer />
   </div>
 )}
 
